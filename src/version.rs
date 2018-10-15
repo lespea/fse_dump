d67f921189593bc66dade248a0b9fa709a90e7a8
Adam Lesperance <lespea@gmail.com>
2018-10-14 21:26:58 -0500

    Fix lifetimes

--- src/version.rs
+++ src/version.rs
@@ -12,50 +12,73 @@ const V1_BYTES: &[u8; 4] = b"1SLD";
 const V2_BYTES: &[u8; 4] = b"2SLD";
 
 #[derive(Debug, PartialEq)]
-struct V1;
+pub struct V1;
 #[derive(Debug, PartialEq)]
-struct V2;
+pub struct V2;
 
 #[derive(Debug, PartialEq)]
-enum Version {
+pub enum Version {
     Ver1(V1),
     Ver2(V2),
 }
 
-trait RecordParser<'a> {
+trait RecordParser {
     const HAS_NODEID: bool;
 
-    fn parse_record(
+    fn parse_record<'a>(
         &self,
         reader: &mut BufRead,
         sbuf: &'a mut Vec<u8>,
-        rec: &'a mut Record<'a>,
-    ) -> io::Result<Option<usize>> {
+    ) -> io::Result<Option<(usize, Record<'a>)>> {
         sbuf.clear();
+        debug!("Reading path");
         let rlen = reader.read_until(b'\0', sbuf)?;
         if rlen == 0 || sbuf[rlen - 1] != b'\0' {
-            println!("End of pages discovered :: {}", rlen);
+            info!("End of pages discovered :: {}", rlen);
             Ok(None)
         } else {
-            rec.path = String::from_utf8_lossy(&sbuf[..rlen - 1]);
-            rec.event_id = reader.read_u64::<BigEndian>()?;
-            rec.flags = flags::parse_bits(reader.read_u32::<BigEndian>()?);
+            debug!("Reading path done");
+            let path = String::from_utf8_lossy(&sbuf[..rlen - 1]);
+            debug!("Found path {}", path);
+            let event_id = reader.read_u64::<BigEndian>()?;
+            debug!("Found event id {}", event_id);
+            let flags = flags::parse_bits(reader.read_u32::<BigEndian>()?);
+            debug!("Found flags {}", flags);
 
             if Self::HAS_NODEID {
-                rec.node_id = Some(reader.read_u64::<LittleEndian>()?);
-                Ok(Some(rlen + 20))
+                debug!("In V2");
+                let node_id = Some(reader.read_u64::<LittleEndian>()?);
+                debug!("Found node id {}", node_id.unwrap());
+                Ok(Some((
+                    rlen + 20,
+                    Record {
+                        path,
+                        event_id,
+                        flags,
+                        node_id,
+                    },
+                )))
             } else {
-                Ok(Some(rlen + 12))
+                debug!("In V1");
+                Ok(Some((
+                    rlen + 20,
+                    Record {
+                        path,
+                        event_id,
+                        flags,
+                        node_id: None,
+                    },
+                )))
             }
         }
     }
 }
 
-impl<'a> RecordParser<'a> for V1 {
+impl RecordParser for V1 {
     const HAS_NODEID: bool = false;
 }
 
-impl<'a> RecordParser<'a> for V2 {
+impl RecordParser for V2 {
     const HAS_NODEID: bool = true;
 }
 
@@ -74,11 +97,10 @@ impl Version {
         &self,
         reader: &mut BufRead,
         sbuf: &'a mut Vec<u8>,
-        rec: &'a mut Record<'a>,
-    ) -> io::Result<Option<usize>> {
+    ) -> io::Result<Option<(usize, Record<'a>)>> {
         match self {
-            Version::Ver1(v) => v.parse_record(reader, sbuf, rec),
-            Version::Ver2(v) => v.parse_record(reader, sbuf, rec),
+            Version::Ver1(v) => v.parse_record(reader, sbuf),
+            Version::Ver2(v) => v.parse_record(reader, sbuf),
         }
     }
 }
