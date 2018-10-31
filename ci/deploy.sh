59f37d92fc3c019190d6cf213a48ac07e57838eb
Adam Lesperance <lespea@users.noreply.github.com>
2018-10-31 04:11:59 -0500

    Fix travis builds (#1)
    Mostly copied from fd

--- ci/deploy.sh
+++ ci/deploy.sh
@@ -1,3 +1,8 @@
 #!/usr/bin/env sh
 
-tar cvf "${PROJECT_NAME}-${TRAVIS_TAG}-${TARGET}.tar" "target/release/fse_dump"
+mkdir -p pfiles
+if [[ "$TRAVIS_OS_NAME" == 'windows' ]]; then
+    cp "target/release/${PROJECT_NAME}.exe" "pfiles/${PROJECT_NAME}-${TRAVIS_TAG}-${TARGET}.exe"
+else
+    cp "target/release/${PROJECT_NAME}" "pfiles/${PROJECT_NAME}-${TRAVIS_TAG}-${TARGET}"
+fi
