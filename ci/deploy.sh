057f7fa463601709155a01977f8e0e0081979228
Adam Lesperance <lespea@gmail.com>
2022-11-22 01:07:31 -0600

    Remove old ci scripts

--- ci/deploy.sh
+++
@@ -1,8 +0,0 @@
-#!/usr/bin/env sh
-
-mkdir -p pfiles
-if [[ "$TRAVIS_OS_NAME" == 'windows' ]]; then
-    cp "target/release/${PROJECT_NAME}.exe" "pfiles/${PROJECT_NAME}-${TRAVIS_TAG}-${TARGET}.exe"
-else
-    cp "target/release/${PROJECT_NAME}" "pfiles/${PROJECT_NAME}-${TRAVIS_TAG}-${TARGET}"
-fi
